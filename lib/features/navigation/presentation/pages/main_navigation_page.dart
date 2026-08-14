ed415b367e041def9f8ae8986756c4d3f0bb0ac3
Ankit Roy <76932622+ankit-roy-greenway@users.noreply.github.com>
2026-08-14 11:47:28 +0530

    Improve refreash functionality for all the tab

--- lib/features/navigation/presentation/pages/main_navigation_page.dart
+++ lib/features/navigation/presentation/pages/main_navigation_page.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import '../../../../core/di/di.dart';
 import '../../../../core/utils/context_extension.dart';
 import '../../../activity/presentation/bloc/activity_bloc.dart';
 import '../../../activity/presentation/bloc/activity_event.dart';
+import '../../../auth/presentation/bloc/auth_bloc.dart';
+import '../../../auth/presentation/bloc/auth_event.dart';
+import '../../../auth/presentation/bloc/auth_state.dart';
+import '../../../friends/presentation/bloc/friends_list_cubit.dart';
+import '../../../home/presentation/bloc/home_bloc.dart';
+import '../../../home/presentation/bloc/home_event.dart';
 
 class MainNavigationPage extends StatelessWidget {
+  static const int groupsTabIndex = 0;
+  static const int friendsTabIndex = 1;
   static const int activityTabIndex = 2;
+  static const int accountTabIndex = 3;
 
   final StatefulNavigationShell navigationShell;
 
@@ -20,8 +30,19 @@ class MainNavigationPage extends StatelessWidget {
       index,
       initialLocation: index == navigationShell.currentIndex,
     );
-    if (index == activityTabIndex) {
-      context.read<ActivityBloc>().add(const RefreshActivity());
+
+    switch (index) {
+      case groupsTabIndex:
+        context.read<HomeBloc>().add(const RefreshHome());
+      case friendsTabIndex:
+        final authState = context.read<AuthBloc>().state;
+        if (authState is Authenticated) {
+          getIt<FriendsListCubit>().load(authState.user.id);
+        }
+      case activityTabIndex:
+        context.read<ActivityBloc>().add(const RefreshActivity());
+      case accountTabIndex:
+        context.read<AuthBloc>().add(const CheckAuthStatus());
     }
   }
 
